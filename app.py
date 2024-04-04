25e415be1467aa5f5c245941b6738e89a7a5662e
DavidKN10 <davidrs23178@gmail.com>
2024-04-04 14:57:41 -0500

    modified table windows and added an exit button

--- app.py
+++ app.py
@@ -7,47 +7,47 @@ import mysql.connector
 def open_election_window():
     election_window = Toplevel(window)
     election_window.title("Election Table")
-    election_window.geometry("500x500")
+    election_window.geometry("900x500")
     Label(election_window, text="testing").pack()
     election_window.grab_set()
 
 def open_committee_window():
     committee_window = Toplevel(window)
     committee_window.title("Committee Table")
-    committee_window.geometry("500x500")
+    committee_window.geometry("900x500")
     Label(committee_window, text="testing").pack()
     committee_window.grab_set()
 
 def open_candidate_window():
     candidate_window = Toplevel(window)
     candidate_window.title("Candidate Table")
-    candidate_window.geometry("500x500")
+    candidate_window.geometry("900x500")
     Label(candidate_window, text="testing").pack()
     candidate_window.grab_set()
 
 def open_contribution_window():
     contribution_window = Toplevel(window)
     contribution_window.title("Contribution Table")
-    contribution_window.geometry("500x500")
+    contribution_window.geometry("900x500")
     Label(contribution_window, text="testing").pack()
     contribution_window.grab_set()
 
 def open_expenditure_window():
     expenditure_window = Toplevel(window)
     expenditure_window.title("Expenditure Table")
-    expenditure_window.geometry("500x500")
+    expenditure_window.geometry("900x500")
     Label(expenditure_window, text="testing").pack()
     expenditure_window.grab_set()
 
 def open_filing_window():
     filing_window = Toplevel(window)
     filing_window.title("Filing Table")
-    filing_window.geometry("500x500")
+    filing_window.geometry("900x500")
     Label(filing_window, text="testing").pack()
     filing_window.grab_set()
 
 def exit_program():
-    return
+    window.destroy()
 
 
 window = Tk()
@@ -62,7 +62,6 @@ TablesFrame = Frame(window)
 TablesFrame.place(x=0, y=120, width=1280, height=400)
 tableFrame = LabelFrame(TablesFrame, font=("", 20, "bold"), text="Select a Table")
 tableFrame.grid(row=0, column=0)
-
 
 election_button = Button(tableFrame, text="Election", font=("", 20),
                          width=40, height=3, command=open_election_window)
@@ -82,6 +81,16 @@ expenditure_button.grid(row=2, column=1)
 filing_button = Button(tableFrame, text="Filing", font=("", 20),
                           width=40, height=3, command=open_filing_window)
 filing_button.grid(row=3, column=1)
+
+
+# ==================== Exit Frame ====================
+ExitFrame = Frame(window)
+ExitFrame.place(x=0, y=550, width=1280, height=400)
+exitFrame = LabelFrame(ExitFrame)
+exitFrame.grid(row=0, column=0)
+
+exit_button = Button(exitFrame, text="Exit", font=("", 20), command=exit_program)
+exit_button.grid(row=1, column=0)
 
 
 window.mainloop()
